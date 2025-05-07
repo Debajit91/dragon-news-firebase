0c76116f2aaa8e42695110fd8dce37c6d33aee39
Debajit91 <debajitroy544@gmail.com>
2025-05-08 00:01:39 +0600

    register functinality implemented

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { createContext, useEffect, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged } from "firebase/auth";
+import app from '../Firebase/firebase.config';
 
-const AuthProvider = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const auth = getAuth(app);
+export const AuthContext = createContext();
+const AuthProvider = ({children}) => {
+    const [user, setUser] = useState(null);
+
+    console.log(user);
+
+    const createUser = (email, password)=>{
+        return createUserWithEmailAndPassword(auth, email, password)
+    };
+
+    useEffect(()=>{
+        const unSubscribe = onAuthStateChanged(auth, (currentUser) =>{
+            setUser(currentUser);
+        });
+        return ()=>{
+            unSubscribe
+        }
+    }, [])
+
+    const authData = {
+        user,
+        setUser,
+        createUser
+    };
+    return <AuthContext value={authData}>{children}</AuthContext>
 };
 
 export default AuthProvider;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,30 +1,52 @@
-import React from 'react';
+import React, { use } from 'react';
 import { Link } from 'react-router';
+import { AuthContext } from '../Provider/AuthProvider';
 
 const Register = () => {
+    const {createUser, setUser} = use(AuthContext)
+    const handleRegister = (e) =>{
+        e.preventDefault();
+        const form = e.target;
+        const name = form.name.value;
+        const photo = form.photo.value;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(name, photo, email, password)
+        createUser(email, password)
+        .then(result =>{
+            const user = result.user;
+            console.log(user);
+            setUser(user)
+        })
+        .catch((error) => {
+            
+            const errorMessage = error.message;
+            alert(errorMessage);
+          });
+    }
     return (
         <div className="flex justify-center items-center min-h-screen">
       <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl py-5">
+      <h2 className="font-semibold text-2xl text-center my-4">Register Your Account</h2>
+        <form onSubmit={handleRegister} className="card-body">
         
-        <div className="card-body">
-        <h2 className="font-semibold text-2xl text-center my-4">Register Your Account</h2>
           <fieldset className="fieldset">
             <label className="label">Name</label>
-            <input type="text" className="input" placeholder="Your Name" />
+            <input name='name' type="text" className="input" required placeholder="Your Name" />
 
             <label className="label">Photo URL</label>
-            <input type="text" className="input" placeholder="Photo URL" />
+            <input name='photo' type="text" className="input" required placeholder="Photo URL" />
 
             <label className="label">Email</label>
-            <input type="email" className="input" placeholder="Email" />
+            <input name='email' type="email" className="input" required placeholder="Email" />
 
             <label className="label">Password</label>
-            <input type="password" className="input" placeholder="Password" />
+            <input name='password' type="password" className="input" required placeholder="Password" />
             
-            <button className="btn btn-neutral mt-4">Register</button>
+            <button type='submit' className="btn btn-neutral mt-4">Register</button>
             <p className="py-4 font-semibold text-center">Already Have An Account?  <Link to="/auth/login" className="text-secondary">Login</Link></p>
           </fieldset>
-        </div>
+        </form>
       </div>
     </div>
     );
